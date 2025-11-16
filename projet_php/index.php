361a077357460e30a5d52db2aa6e10f23a85ad2c
KathyMunoz <katherine.munoz.moran@gmail.com>
2025-11-16 20:38:01 +0100

    fini 18 ET 19

--- projet_php/index.php
+++ projet_php/index.php
@@ -266,7 +266,39 @@ function displayPetit ($tab) {
 echo displayPetit(([1,19,2,3,4]));
 
 echo'<p> Exercice 18  </p>';
+//Créer un script qui affiche les nombres de 1 -> 5 (méthode echo). Version FOR et version WHILE
+echo'<p> Version FOR  </p>';
+for ($i=1 ; $i<6 ; $i++) {
+    echo '<p> '.$i. '</p>';
+} 
+echo'<p> Version WHILE  </p>';
+$i=0;
+while ($i<5) {
+    $i++;
+    echo '<p> '.$i. '</p>';
+}
+
 echo'<p> Exercice 19  </p>';
+/*Ecrire une fonction qui prend un nombre en paramètre (variable $nbr), et qui ensuite affiche les dix nombres
+suivants. Par exemple, si la valeur de nbr équivaut à : 17, la fonction affichera les nombres de 18 à 27 (méthode
+echo). Version FOR et version WHILE */
+echo'<p> Version FOR  </p>';
+function suite ($nbr) {
+    for ($i=1; $i<=10; $i++) {
+        echo '<p>'.($nbr + $i).'</p>';
+    }
+}
+suite(10);
+
+echo'<p> Version WHILE  </p>';
+function suiteBis ($nbr){
+    $i = 0;
+    while($i<=10) {
+        $i++;
+        echo '<p>'.($nbr+ $i).'</p>';
+    }
+}
+suiteBis(20);
 
 
 
